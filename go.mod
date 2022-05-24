e8cb5d6e4ce51572983c70bbfa038823793358f7
JoSe78 <jose.clavero.anderica@gmail.com>
2022-05-24 16:24:47 +0200

    created

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jose78/gansible
 
 go 1.18
 
-require gopkg.in/yaml.v3 v3.0.0 // indirect
+require (
+	golang.org/x/exp v0.0.0-20220518171630-0b5c67f07fdf // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,30 +1,64 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"gopkg.in/yaml.v3"
+        "gopkg.in/yaml.v3"
+        "log"
+        "os"
+"golang.org/x/exp/maps"
+"strings"
 )
-
 
 type Generic interface{}
 
-func main(){
-     loadContent("./demo.yml")
+func main() {
+        containerData := loadContent("test/demo.yml")
+        log.Println(containerData["tasks"])
+        for _, item := range containerData["tasks"].([]interface{}) {
+             evaluateTask(item)
+        }
 }
 
-func checkError(e error, contents...string) {
-    if e != nil {
-	fmt.Println(contents)
-        panic(e)
-    }
+func checkError(e error, description string) {
+        if e != nil {
+                log.Printf("%s -> %w \n", description, e)
+                panic(e)
+        }
 }
 
-func loadContent(path string){
-     data, err := os.ReadFile(path)
-     checkError(err, "Reading file: " + path)
-     containerData := map[string]Generic{}
-     err2 := yaml.Unmarshal(data, &containerData)
-     checkError(err2, "Unmarshal yaml from " + path)
-     fmt.Println(containerData)
+func loadContent(path string) map[string]Generic {
+        data, err := os.ReadFile(path)
+        checkError(err, "Reading file: "+path)
+        containerData := map[string]Generic{}
+	      err2 := yaml.Unmarshal(data, &containerData)
+        checkError(err2, "Unmarshal yaml from "+path)
+        log.Println(containerData)
+        return containerData
+}
+
+func evaluateTask(task interface{}){
+  selectTask := func (clouserTask map[string]interface{}){
+
+    maps.Keys(clouserTask)
+    keys := strings.Join(maps.Keys(clouserTask)[:]," ")
+   log.Println(keys)
+   log.Println(strings.Trim(keys, "name"))
+  }
+  selectTask(task.(map[string]interface{}))
+}
+
+type Task struct {
+        Name string
+        body Executable
+}
+
+type Executable interface {
+        Execute()
+}
+
+type Debug struct {
+        msg string
+}
+
+func (task *Debug) Execute() {
+        log.Println(task.msg)
 }
